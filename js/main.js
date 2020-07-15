bf7f9310cd7559f466f1e576cb982c5a8a48fbf2
nr <nagyroland.1989@hotmail.com>
2020-07-15 20:44:24 +0200

    started to make the js for calculating price

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function calcAmount() {
+    let price = 1200;
+    let extraPrice = document.querySelector("input[name=extraOrder]:checked").value;
+    let sauceType = document.querySelector("#sauceList").value;
+    let quantity = document.querySelector("input[name='quantity']").value;
+    
+}
