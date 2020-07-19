770f0377af3fae0ec776824d7eaf9a9f777e9e59
nr <nagyroland.1989@hotmail.com>
2020-07-19 16:38:34 +0200

    fixed js script link, added checks for inputs with if else statements to make sure the correct values are entered.

--- js/orders.js
+++ js/orders.js
@@ -1,9 +1,28 @@
 function calcAmount() {
+    let name = document.querySelector("#inputName").value.trim();
+    let email = document.querySelector("#inputEmail").value.trim();
+    let address = document.querySelector("#inputAddress").value.trim();
+    let comment = document.querySelector("#inputComment").value.trim();
+
     let price = 1200;
     let extraPrice = parseInt(document.querySelector("input[name=extraOrder]:checked").value);
     let sauceType = parseInt(document.querySelector("#sauceList").value);
     let quantity = parseInt(document.querySelector("input[name='quantity']").value);
-    let totalPrice = quantity*(price+extraPrice+sauceType);
-    let showAmount = document.querySelector("span.show-amount");
-    showAmount.innerHTML =totalPrice;
+
+    if (!name || name.length < 2) {
+        alert("Név megadása kötelező!");
+    } else if (!email || email.indexOf("@") < 0 || email.indexOf(".") < 1) {
+        alert("Invalid email cím!");
+    } else if (address.length < 10) {
+        alert("Kérjük pontos címet adj meg!");
+    } else if (comment.indexOf("<") >=0 && comment.indexOf(">") >=0) {
+        alert("A megjegyzésben nem megengedett a HTML használata.");
+    } else if (!quantity || quantity < 1 || quantity > 10) {
+        alert("A rendelés mennyisége minimum 1, maximum 10 darab.")
+    } else {
+        let totalPrice = quantity*(price+extraPrice+sauceType);
+        let showAmount = document.querySelector("span.show-amount");
+        showAmount.innerHTML =totalPrice;
+    }
+
 }
